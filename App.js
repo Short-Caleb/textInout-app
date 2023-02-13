ae816b87ed2e9709de63d6afcd30e86ebf264e12
Caleb Short <shortcaleb@gmail.com>
2023-02-13 14:35:01 -0500

    textinput first commit

--- App.js
+++ App.js
@@ -1,12 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, SafeAreaView, Button, FlatList } from 'react-native';
 
 export default function App() {
+
+const [userInput, setUserInput] = useState('');
+
+const userData = {
+  messages: [
+    {text: 'first user input'},
+    {text: 'next used input'}
+  ]
+}
+
+const addMessage = () => { 
+  userData.messages.push({text: userInput});
+  setUserInput(''); 
+  console.log(userData.messages)
+}
+
+const Message = ({item}) => {
+ const renderItem = ({item}) => (
+ <View>
+    <Text>{item.text}</Text>
+  </View>
+)
+return (
+  <FlatList
+  data={userData.messages}
+  renderItem={renderItem}
+  keyExtractor={(item, index) => index }
+  />
+)
+}
+
+
+const storeData = async () => {
+  try {
+    const jsonValue = JSON.stringify(userInput)
+    await AsyncStorage.setItem('@storage_Key', jsonValue)
+  } catch (e) {
+    // saving error
+  }
+}
+
+
+const getData = async () => {
+  try {
+    const jsonValue = await AsyncStorage.getItem('@storage_Key')
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch(e) {
+    // error reading value
+  }
+}
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <SafeAreaView>
+      <Text>display something </Text>
+      <TextInput
+      style={styles.input}
+      onChangeText={setUserInput}
+      value={userInput} />
+
+      <Button
+      title='Submit'
+      onPress={addMessage}
+      />
+     <Message/>
+   </SafeAreaView>
   );
 }
 
@@ -17,4 +77,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  }
 });
